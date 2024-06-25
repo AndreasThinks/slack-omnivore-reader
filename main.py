8d9cdd0197e34edba71c03d7e590300c86d5bab9
AndreasThinks <avarotsis@no10.gov.uk>
2024-06-25 12:46:08 +0100

    debug

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from config import settings
 from slack_handlers import app as slack_app
 from utils import setup_rate_limiter, setup_logging
 
-# Call this function at the start of your application
-setup_logging()
+
+# Set up logging
+logger = setup_logging()
 
 # Initialize FastAPI app
 fastapi_app = FastAPI()
