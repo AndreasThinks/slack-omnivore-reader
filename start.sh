6afb396d9612d877082a570978a979a2544cd66a
AndreasThinks <avarotsis@no10.gov.uk>
2024-07-31 10:59:41 +0100

    reduced delay to 20 secs

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@
 python summariser/newsletter_creator.py
 
 # Wait for 3 minutes
-echo "Waiting for 90 seconds..."
-sleep 90
+echo "Waiting for 20 seconds..."
+sleep 20
 
 # Start the web process
 uvicorn app:app --host=0.0.0.0 --port=$PORT
